e1b5fd524bd02a4488f23b72a8d706f769e878ee
Wihctoh <rivertoxi911@gmail.com>
2023-06-03 11:07:24 +0300

    fix

--- src/db.js
+++ src/db.js
@@ -1,12 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  // password: process.env.PWD,
-  password: "12345678",
+  password: process.env.PWD,
   port: process.env.PORT_DB,
   user: process.env.USER,
   database: process.env.DB,
   host: process.env.HOST,
 });
-console.log(process.env.PWD);
 module.exports = pool;
